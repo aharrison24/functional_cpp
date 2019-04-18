e0d084ff69069002b1432e100bd3e035e7ed6afe
Alastair Harrison <aharrison24@gmail.com>
2019-04-18 10:58:09 +0100

    Add any_of implemented in terms of foldl

--- chapter2.cpp
+++ chapter2.cpp
@@ -1,3 +1,5 @@
+#include <catch2/catch.hpp>
+
 #include <utility>
 
 // Implementing some common algorithms in terms of std::accumulate
@@ -15,4 +17,47 @@ constexpr auto foldl(ForwardIter first,
   return init_value;
 }
 
+template <typename ForwardIter, typename Predicate>
+constexpr bool any_of(ForwardIter first, ForwardIter last, Predicate p) {
+  return foldl(
+      first, last, false,
+      [p = std::move(p)](bool init, auto const& v) { return init | p(v); });
+}
+
 }  // namespace fcpp
+
+template <typename T>
+auto greater_than(T lim) {
+  return [lim = std::move(lim)](auto const& v) { return v > lim; };
+}
+
+CATCH_SCENARIO("any_of") {
+  CATCH_GIVEN("An empty collection") {
+    std::vector<int> input;
+    CATCH_THEN("any_of returns false") {
+      CATCH_REQUIRE(!fcpp::any_of(begin(input), end(input), greater_than(5)));
+    }
+  }
+
+  CATCH_GIVEN("A collection containing only items passing the predicate") {
+    std::vector<int> input{6, 7, 8, 9};
+    CATCH_THEN("any_of returns true") {
+      CATCH_REQUIRE(fcpp::any_of(begin(input), end(input), greater_than(1)));
+    }
+  }
+
+  CATCH_GIVEN("A collection containing only items not-passing the predicate") {
+    std::vector<int> input{1, 2, 3, 4};
+    CATCH_THEN("any_of returns false") {
+      CATCH_REQUIRE(!fcpp::any_of(begin(input), end(input), greater_than(5)));
+    }
+  }
+
+  CATCH_GIVEN(
+      "A collection with only a subset of items passing the predicate") {
+    std::vector<int> input{1, 6, 2, 10, 100};
+    CATCH_THEN("any_of returns true") {
+      CATCH_REQUIRE(fcpp::any_of(begin(input), end(input), greater_than(5)));
+    }
+  }
+}
