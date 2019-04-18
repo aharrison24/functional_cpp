5f4e8e4644de5669dc7f1fd3260505cb9dc5284f
Alastair Harrison <aharrison24@gmail.com>
2019-04-18 10:58:59 +0100

    Add find_if implemented in terms of foldl

--- chapter2.cpp
+++ chapter2.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
 
+#include <iterator>
 #include <utility>
 
 // Implementing some common algorithms in terms of std::accumulate
@@ -29,6 +30,23 @@ constexpr bool all_of(ForwardIter first, ForwardIter last, Predicate p) {
   return foldl(first, last, true, [p = std::move(p)](bool init, auto const& v) {
     return init && p(v);
   });
+}
+
+template <typename ForwardIter, typename Predicate>
+constexpr auto find_if(ForwardIter first, ForwardIter last, Predicate p) {
+  struct accumulator_t {
+    typename std::iterator_traits<ForwardIter>::difference_type count{0};
+    bool found{false};
+  };
+
+  auto increment_if_not = [p = std::move(p)](accumulator_t init,
+                                             auto const& val) {
+    return (init.found || p(val)) ? accumulator_t{init.count, true}
+                                  : accumulator_t{init.count + 1, false};
+  };
+
+  auto result = foldl(first, last, accumulator_t{}, increment_if_not);
+  return std::next(first, result.count);
 }
 
 }  // namespace fcpp
@@ -96,6 +114,33 @@ CATCH_SCENARIO("all_of") {
     std::vector<int> input{1, 6, 2, 10, 100};
     CATCH_THEN("all_of returns false") {
       CATCH_REQUIRE(!fcpp::all_of(begin(input), end(input), greater_than(5)));
+    }
+  }
+}
+
+CATCH_SCENARIO("find_if") {
+  CATCH_GIVEN("An empty collection") {
+    std::vector<int> input;
+    CATCH_THEN("find_if returns end iterator") {
+      CATCH_REQUIRE(fcpp::find_if(begin(input), end(input), greater_than(5)) ==
+                    end(input));
+    }
+  }
+
+  CATCH_GIVEN("A collection containing only items passing the predicate") {
+    std::vector<int> input{6, 7, 8, 9};
+    CATCH_THEN("find_if returns an iterator to the first") {
+      CATCH_REQUIRE(fcpp::find_if(begin(input), end(input), greater_than(5)) ==
+                    begin(input));
+    }
+  }
+
+  CATCH_GIVEN(
+      "A collection where the initial elements don't pass the predicate") {
+    std::vector<int> input{1, 2, 3, 4, 5, 6, 7, 8};
+    CATCH_THEN("find_if returns an iterator to the first one that does") {
+      CATCH_REQUIRE(fcpp::find_if(begin(input), end(input), greater_than(5)) ==
+                    std::next(begin(input), 5));
     }
   }
 }
